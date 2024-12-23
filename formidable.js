b3460a3ee2e372b46d28291ab48c5a5ae48cb8c9
陳博賢 <chenboxian@chenboxiandeMacBook-Air.local>
2024-12-23 11:37:14 +0800

    new

--- formidable.js
+++ formidable.js
@@ -1,14 +1,27 @@
+const http = require("http");
 const { Storage } = require("@google-cloud/storage");
+const formidable = require("formidable");
+const fs = require("fs");
+
+const port = process.env.PORT || 8080;
+
+// 配置 GCS
 const storage = new Storage({
   credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS), // 從環境變數解析 JSON
 });
-const bucketName = "uploadfile-storage";
+const bucketName = "uploadfile-storage"; // 請替換為你的 Bucket 名稱
 
 http
   .createServer(function (req, res) {
     if (req.url == "/fileupload") {
       var form = new formidable.IncomingForm();
       form.parse(req, function (err, fields, files) {
+        if (err) {
+          res.write("Error in file upload: " + err.message);
+          res.end();
+          return;
+        }
+
         var uploadedFile = files.filetoupload[0];
         var filePath = uploadedFile.filepath;
         var fileName = uploadedFile.originalFilename;
